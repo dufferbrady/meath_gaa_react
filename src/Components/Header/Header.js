e3457125121a916f53d291d61273d4f2b2d5deab
dufferbrady <92andrew.brady@gmail.com>
2019-03-07 21:21:06 -0800

    added material UI for styling header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button'
 
 import classes from './Header.module.css'
 
 const Header = () => {
     return (
-        <header className={ classes.Header }>
-            <div>Icon</div>
-            <div>Menu items</div>
-        </header>
+        <AppBar 
+        className={ classes.Header }
+        style={{
+            background: '#259C41',
+            boxShadow: 'none',
+        }}>
+            <Toolbar>
+                <div className={ classes.Icon }>Meath Icon</div>
+                <Link className={ classes.Link } to="/Fixtures">
+                    <Button color="inherit">Fixtures</Button>
+                </Link>
+                <Link className={ classes.Link } to="/The-Team">
+                    <Button color="inherit">The Team</Button>
+                </Link>
+            </Toolbar>
+        </AppBar>
     );
 };
 
