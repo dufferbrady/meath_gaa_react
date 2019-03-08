8c5052d5f04fc3c59bb56d7391d09a14819258dc
dufferbrady <92andrew.brady@gmail.com>
2019-03-07 22:19:15 -0800

    changed pascalCase

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-import Meath_Icon from '../UI/Icon/Icon'
-import Meath_Logo from '../../Resources/Icons/meath-gaa-logo.png'
+import MeathIcon from '../UI/Icon/Icon'
+import MeathLogo from '../../Resources/Icons/meath-gaa-logo.png'
 import classes from './Footer.module.css'
 
 const Footer = () => {
     return (
         <footer>
             <div className={ classes.Footer_Logo}>
-                <Meath_Icon 
-                url={ `${Meath_Logo}` }
+                <MeathIcon 
+                url={ `${MeathLogo}` }
                 link={ false }
                 width="50px"
                 height="50px"/>
