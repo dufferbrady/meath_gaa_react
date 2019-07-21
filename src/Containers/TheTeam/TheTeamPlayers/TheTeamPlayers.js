52a512dbfeea0323ab4357da8f78d065917e035f
dufferbrady <92andrew.brady@gmail.com>
2019-07-21 14:45:17 -0700

    tab bar now shows players based on positions

--- src/Containers/TheTeam/TheTeamPlayers/TheTeamPlayers.js
+++ src/Containers/TheTeam/TheTeamPlayers/TheTeamPlayers.js
@@ -15,6 +15,7 @@ import { firebasePlayers } from '../../../Firebase';
 import { getFirebaseDataHandler, playerSeperator } from '../../../Components/misc/helpers';
 import Modal from '../../../Components/UI/Modal/Modal';
 import Tag from '../../../Components/UI/Tag/Tag';
+import Aux from '../../../HOC/Auxillary/Auxillary';
 
 import classes from './TheTeamPlayers.module.css';
 
@@ -30,7 +31,8 @@ class TheTeamPlayers extends Component {
         selectPlayer: null,
         selectPlayerSuccess: false,
         modalLoading: false,
-        showBackdrop: false
+        showBackdrop: false,
+        value: 0
     }
 
     componentDidMount() {
@@ -65,9 +67,21 @@ class TheTeamPlayers extends Component {
         })
     }
 
+    changePlayersHandler = (event, newPlayers) => {
+        this.setState({
+            value: newPlayers
+        })
+        console.log(newPlayers)
+    }
+
     render() {
         let players = null;
+        let goalkeepers = null;
+        let defenders = null;
+        let midfielders = null;
+        let forwards = null;
         let modal = null;
+
         if (this.state.loading) {
             players = (
                 <Spinner
@@ -76,17 +90,124 @@ class TheTeamPlayers extends Component {
                     marginTop="100px" />
             )
         } else {
+            goalkeepers = (
+                <Aux>
+                    {
+                        this.state.goalkeepers.map((player, i) => (
+                            <Card
+                                onClick={() => this.toggleBackdropHandler(true, player.id)}
+                                className={classes.Card}
+                                key={i}
+                                style={{
+                                    width: '20%',
+                                    margin: '25px',
+                                }}>
+                                <CardContent style={{
+                                    display: 'flex',
+                                    flexFlow: 'row nowrap',
+                                    justifyContent: 'flex-start',
+                                    alignItems: 'center'
+                                }}>
+                                    <Avatar src={player.imageURL} style={{ width: '75px', height: '75px', marginRight: '10px' }} />
+                                    <Typography variant="h6" gutterBottom>
+                                        {player.name}
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        ))
+                    }
+                </Aux>
+            )
+            defenders = (
+                <Aux>
+                    {
+                        this.state.defenders.map((player, i) => (
+                            <Card
+                                className={classes.Card}
+                                onClick={() => this.toggleBackdropHandler(true, player.id)}
+                                key={i}
+                                style={{
+                                    width: '20%',
+                                    margin: '25px'
+                                }}>
+                                <CardContent style={{
+                                    display: 'flex',
+                                    flexFlow: 'row nowrap',
+                                    justifyContent: 'flex-start',
+                                    alignItems: 'center'
+                                }}>
+                                    <Avatar src={player.imageURL} style={{ width: '75px', height: '75px', marginRight: '10px' }} />
+                                    <Typography variant="h6" gutterBottom>
+                                        {player.name}
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        ))
+                    }
+                </Aux>
+            )
+            midfielders = (
+                <Aux>
+                    {
+                        this.state.midfielders.map((player, i) => (
+                            <Card
+                                className={classes.Card}
+                                onClick={() => this.toggleBackdropHandler(true, player.id)}
+                                key={i}
+                                style={{
+                                    width: '20%',
+                                    margin: '25px'
+                                }}>
+                                <CardContent style={{
+                                    display: 'flex',
+                                    flexFlow: 'row nowrap',
+                                    justifyContent: 'flex-start',
+                                    alignItems: 'center'
+                                }}>
+                                    <Avatar src={player.imageURL} style={{ width: '75px', height: '75px', marginRight: '10px' }} />
+                                    <Typography variant="h6" gutterBottom>
+                                        {player.name}
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        ))
+                    }
+                </Aux>
+            )
+            forwards = (
+                <Aux>
+                    {
+                        this.state.forwards.map((player, i) => (
+                            <Card
+                                className={classes.Card}
+                                onClick={() => this.toggleBackdropHandler(true, player.id)}
+                                key={i}
+                                style={{
+                                    width: '20%',
+                                    margin: '25px'
+                                }}>
+                                <CardContent style={{
+                                    display: 'flex',
+                                    flexFlow: 'row nowrap',
+                                    justifyContent: 'flex-start',
+                                    alignItems: 'center'
+                                }}>
+                                    <Avatar src={player.imageURL} style={{ width: '75px', height: '75px', marginRight: '10px' }} />
+                                    <Typography variant="h6" gutterBottom>
+                                        {player.name}
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        ))
+                    }
+                </Aux>
+            )
             players = (
-                <Paper style={{
-                    display: 'flex',
-                    flexFlow: 'row wrap',
-                    justifyContent: 'flex-start',
-                    paddingBottom: '25px'
-                }}>
+                <Aux>
                     <SectionBar
                         sectionName={'Goalkeepers'}
+                        onClick={() => console.log('clicked')}
                         style={{
-                            margin: '0 0 15px 0',
                             width: '100%',
                             backgroundColor: '#F9FAFB',
                             padding: '15px 40px',
@@ -95,32 +216,10 @@ class TheTeamPlayers extends Component {
                             color: '#747D8D',
                             borderBottom: '2px solid #E8EAEC'
                         }} />
-                    {this.state.goalkeepers.map((player, i) => (
-                        <Card
-                            onClick={() => this.toggleBackdropHandler(true, player.id)}
-                            className={classes.Card}
-                            key={i}
-                            style={{
-                                width: '20%',
-                                margin: '5px 25px',
-                            }}>
-                            <CardContent style={{
-                                display: 'flex',
-                                flexFlow: 'row nowrap',
-                                justifyContent: 'flex-start',
-                                alignItems: 'center'
-                            }}>
-                                <Avatar src={player.imageURL} style={{ width: '75px', height: '75px', marginRight: '10px' }} />
-                                <Typography variant="h6" gutterBottom>
-                                    {player.name}
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                    ))}
+                    {goalkeepers}
                     <SectionBar
                         sectionName={'Defenders'}
                         style={{
-                            margin: '15px 0',
                             width: '100%',
                             backgroundColor: '#F9FAFB',
                             padding: '15px 40px',
@@ -129,32 +228,10 @@ class TheTeamPlayers extends Component {
                             color: '#747D8D',
                             border: '2px solid #E8EAEC'
                         }} />
-                    {this.state.defenders.map((player, i) => (
-                        <Card
-                            className={classes.Card}
-                            onClick={() => this.toggleBackdropHandler(true, player.id)}
-                            key={i}
-                            style={{
-                                width: '20%',
-                                margin: '5px 25px',
-                            }}>
-                            <CardContent style={{
-                                display: 'flex',
-                                flexFlow: 'row nowrap',
-                                justifyContent: 'flex-start',
-                                alignItems: 'center'
-                            }}>
-                                <Avatar src={player.imageURL} style={{ width: '75px', height: '75px', marginRight: '10px' }} />
-                                <Typography variant="h6" gutterBottom>
-                                    {player.name}
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                    ))}
+                    {defenders}
                     <SectionBar
                         sectionName={'Midfielders'}
                         style={{
-                            margin: '15px 0',
                             width: '100%',
                             backgroundColor: '#F9FAFB',
                             padding: '15px 40px',
@@ -163,32 +240,10 @@ class TheTeamPlayers extends Component {
                             color: '#747D8D',
                             border: '2px solid #E8EAEC'
                         }} />
-                    {this.state.midfielders.map((player, i) => (
-                        <Card
-                            className={classes.Card}
-                            onClick={() => this.toggleBackdropHandler(true, player.id)}
-                            key={i}
-                            style={{
-                                width: '20%',
-                                margin: '5px 25px',
-                            }}>
-                            <CardContent style={{
-                                display: 'flex',
-                                flexFlow: 'row nowrap',
-                                justifyContent: 'flex-start',
-                                alignItems: 'center'
-                            }}>
-                                <Avatar src={player.imageURL} style={{ width: '75px', height: '75px', marginRight: '10px' }} />
-                                <Typography variant="h6" gutterBottom>
-                                    {player.name}
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                    ))}
+                    {midfielders}
                     <SectionBar
                         sectionName={'Forwards'}
                         style={{
-                            margin: '15px 0',
                             width: '100%',
                             backgroundColor: '#F9FAFB',
                             padding: '15px 40px',
@@ -197,29 +252,8 @@ class TheTeamPlayers extends Component {
                             color: '#747D8D',
                             border: '2px solid #E8EAEC'
                         }} />
-                    {this.state.forwards.map((player, i) => (
-                        <Card
-                            className={classes.Card}
-                            onClick={() => this.toggleBackdropHandler(true, player.id)}
-                            key={i}
-                            style={{
-                                width: '20%',
-                                margin: '5px 25px',
-                            }}>
-                            <CardContent style={{
-                                display: 'flex',
-                                flexFlow: 'row nowrap',
-                                justifyContent: 'flex-start',
-                                alignItems: 'center'
-                            }}>
-                                <Avatar src={player.imageURL} style={{ width: '75px', height: '75px', marginRight: '10px' }} />
-                                <Typography variant="h6" gutterBottom>
-                                    {player.name}
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                    ))}
-                </Paper>
+                    {forwards}
+                </Aux>
             )
         }
         if (!this.state.selectPlayer) {
@@ -279,16 +313,29 @@ class TheTeamPlayers extends Component {
                         className={classes.Text}
                         indicatorColor="secondary"
                         variant="fullWidth"
+                        value={this.state.value}
+                        onChange={this.changePlayersHandler}
                     >
-                        <Tab style={{ fontSize: '15px' }} label="Full Team" />
-                        <Tab style={{ fontSize: '15px' }} label="Goalkeepers" />
-                        <Tab style={{ fontSize: '15px' }} label="Defenders" />
-                        <Tab style={{ fontSize: '15px' }} label="Midfielders" />
-                        <Tab style={{ fontSize: '15px' }} label="Forwards" />
+                        <Tab style={{ fontSize: '15px' }} label="Full Team" value={0} />
+                        <Tab style={{ fontSize: '15px' }} label="Goalkeepers" value={1} />
+                        <Tab style={{ fontSize: '15px' }} label="Defenders" value={2} />
+                        <Tab style={{ fontSize: '15px' }} label="Midfielders" value={3} />
+                        <Tab style={{ fontSize: '15px' }} label="Forwards" value={4} />
                     </Tabs>
                 </AppBar>
                 {modal}
-                {players}
+                <Paper style={{
+                    display: 'flex',
+                    flexFlow: 'row wrap',
+                    justifyContent: 'flex-start',
+                    //paddingBottom: '25px'
+                }}>
+                    {this.state.value === 0 && players}
+                    {this.state.value === 1 && goalkeepers}
+                    {this.state.value === 2 && defenders}
+                    {this.state.value === 3 && midfielders}
+                    {this.state.value === 4 && forwards}
+                </Paper>
             </div>
         );
     }
