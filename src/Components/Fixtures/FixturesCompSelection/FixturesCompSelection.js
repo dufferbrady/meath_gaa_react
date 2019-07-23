8a0c07d4f1217c94aae441dce679be3c0ecb2487
dufferbrady <92andrew.brady@gmail.com>
2019-07-23 00:04:00 -0700

    added buttons to the fixture selection

--- src/Components/Fixtures/FixturesCompSelection/FixturesCompSelection.js
+++ src/Components/Fixtures/FixturesCompSelection/FixturesCompSelection.js
@@ -1,14 +1,38 @@
 import React from 'react';
 
-import classes from './FixturesCompSelection.module.css'
+import classes from './FixturesCompSelection.module.css';
 
-import Paper from '@material-ui/core/Paper'
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
 
 const FixturesCompSelection = () => {
     return (
         <Paper className={classes.Paper}>
-            <span>Competition Selection</span>
-            
+            <span className={classes.Fixtures_Header}>
+                Competition Selection
+            </span>
+            <div className={classes.Buttons_Container}>
+                <Button 
+                variant="contained"
+                style={{
+                    marginRight: '5%',
+                    fontSize: '75%',
+                    backgroundColor: '#259C41',
+                    color: 'white'
+                }}>
+                    League
+                </Button>
+                <Button 
+                variant="contained"
+                style={{
+                    marginLeft: '5%',
+                    fontSize: '75%',
+                    backgroundColor: '#259C41',
+                    color: 'white'
+                }}>
+                    Championship
+                </Button>
+            </div>
         </Paper>
     );
 };
